5b6fadd64bb909d41a4a20fd01d001e57f5e269b
syerahaddad <syerahaddad14@gmail.com>
2024-01-04 15:48:30 +0700

    First Commit

---
+++ profil.js
@@ -0,0 +1,30 @@
+document.writeln('<div class="profile">');
+document.writeln('    <div class="title">Biodata</div>');
+document.writeln('    <div class="profile-info">');
+document.writeln('        <div class="profile-item">Nama: Sayida Syahira</div>');
+document.writeln('        <div class="profile-item">Alamat: Jakarta</div>');
+document.writeln('        <div class="profile-item">Kelas: 2SE3</div>');
+document.writeln('    </div>');
+document.writeln('</div>');
+document.writeln('<br>');
+
+document.writeln('<div class="title">Hobi</div>');
+document.writeln('<div class="list">');
+document.writeln('    <ul>');
+document.writeln('        <li class="hobi">Menonton Film</li>');
+document.writeln('        <li class="hobi">Mendengarkan Musik</li>');
+document.writeln('        <li class="hobi">Melukis</li>');
+document.writeln('    </ul>');
+document.writeln('</div>');
+
+document.writeln('<div class="title">Makanan Favorit</div>');
+document.writeln('<div class="list">');
+document.writeln('    <ul>');
+document.writeln('        <li class="makan">Sushi</li>');
+document.writeln('        <li class="makan">Ayam Goreng</li>');
+document.writeln('        <li class="makan">Pasta</li>');
+document.writeln('    </ul>');
+document.writeln('</div>');
+
+document.writeln('<div class="title">Kata-kata</div>');
+document.writeln('<div class="quote">"There is no such thing as bad people. We’re all just people who sometimes do bad things."</div>');
